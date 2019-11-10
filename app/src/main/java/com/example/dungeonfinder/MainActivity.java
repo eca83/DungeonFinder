1ed147e34fd42297b2e253b8a569672fd3b049e4
knishibe <knishibeckingham@gmail.com>
2019-11-10 15:42:23 -0800

    Added page transition

--- app/src/main/java/com/example/dungeonfinder/MainActivity.java
+++ app/src/main/java/com/example/dungeonfinder/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.dungeonfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Button button_mainMenu = (Button) findViewById(R.id.button_mainMenu);
+        ImageButton button_questMenu =  findViewById(R.id.button_questMenu);
+
+//        button_mainMenu.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View activity_main) {
+//                startActivity(new Intent(MainActivity.this, YourNewActivity.class));
+//            }
+//        });
+
+        button_questMenu.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View activity_main) {
+                startActivity(new Intent(MainActivity.this, QuestMenuActivity.class));
+            }
+        });
+
     }
+
 }
