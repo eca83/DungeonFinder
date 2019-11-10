b67b2f6d1c335bdba33bc8dedaab41e4d93e215c
EmilyChen <eca83@sfu.ca>
2019-11-10 12:18:31 -0800

    database test

--- app/src/main/java/com/example/dungeonfinder/MainActivity.java
+++ app/src/main/java/com/example/dungeonfinder/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.dungeonfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.DatabaseErrorHandler;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // create the database
+        myDb = new DatabaseHelper(this);
     }
 }
